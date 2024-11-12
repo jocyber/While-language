632a04046e16a9cc40577ceedc46ff2fceb9c226
Jordan Harman <joharmh@amazon.com>
2024-11-12 08:38:30 -0600

    Add while block and begin expression statement

--- while.rkt
+++ while.rkt
@@ -2,22 +2,13 @@
 
 (require racket/match
          racket/function
+         "ast-nodes.rkt"
          )
 
 ; BNF grammar
 ; a ::= n | x | a1 + a2 | a1 * a2 | a1 - a2
 ; b ::= ~b | true | false | a1 = a2 | a1 <= a2 | b1 && b2
 ; S ::= S ; S | x := a | skip | while b do S | if b then S1 else S2
-
-(struct Numeral (n) #:transparent)
-(struct Boolean (b) #:transparent)
-(struct Identifier (x) #:transparent)
-(struct BinaryExpr (op a1 a2) #:transparent)
-(struct UnaryExpr (op a) #:transparent)
-(struct While (b s) #:transparent)
-(struct Begin (ss) #:transparent)
-(struct If (b s1 s2) #:transparent)
-(struct Skip () #:transparent)
 
 ; semantic function for WHILE 
 (define ((interpret expr) env)
@@ -26,11 +17,8 @@
     [(Boolean b) b]
     [(Skip) (void)]
     [(Identifier x) 
-     (hash-ref env x (thunk (error (format "Undefined variable in scope [~s]" x))))]
+     (hash-ref env x (thunk (error (format "Undefined variable in scope: ~s" x))))]
     [(BinaryExpr ':= id e)
-     (when (hash-has-key? env id)
-       (error (format "~s is already a bound identifer" id)))
-
      (hash-set! env id ((interpret e) env))]
     [(BinaryExpr op a1 a2) 
      (let ([v1 ((interpret a1) env)]
@@ -40,11 +28,20 @@
     [(If condition consequent alternate)
      (if ((interpret condition) env)
          ((interpret consequent) env)
-         ((interpret alternate) env))]))
- 
+         ((interpret alternate) env))]
+    [(Begin ss) (for/last ([s ss]) ((interpret s) env))]
+    [(While condition s)
+     (let while ()
+       (cond 
+         [((interpret condition) env) 
+          ((interpret s) env)
+          (while)]
+         [else (void)]))]))
+
 (module+ test
   (require rackunit)
 
+  ; arithmetic and variable access
   (check-eq? 3 ((interpret (Numeral 3)) (make-hash)) "numerals are self-evaluated")
   (check-eq? 4
              ((interpret 
@@ -52,5 +49,15 @@
                (make-hash '([x . 3])))
              "addition produces correct sum")
   (check-exn exn:fail? (thunk ((interpret (Identifier 'x)) (make-hash))) "error on uninitialized variable access")
+
+  ; while 
+  (check-eq? 4 
+             ((interpret 
+                (Begin (list (BinaryExpr ':= 'x (Numeral 3))
+                             (While (BinaryExpr < (Identifier 'x) (Numeral 4))
+                                    (BinaryExpr ':= 'x (BinaryExpr + (Identifier 'x) (Numeral 1))))
+                             (Identifier 'x))))
+              (make-hash))
+             "variables are not shadowed in while block")
   )
 
